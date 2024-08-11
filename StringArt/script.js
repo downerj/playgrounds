b05147039afa7924b61438fd93235a0acc6bd6ba
Jimmy Downer <jamesdanieldowner@gmail.com>
2024-08-11 11:18:32 -0600

    Add string art

---
+++ StringArt/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  const cvs = document.getElementById('cvs');
+  const ctx = cvs.getContext('2d');
+
+  /**
+   *
+   */
+  const resizeCanvas = () => {
+    cvs.width = cvs.parentElement.clientWidth;
+    cvs.height = cvs.parentElement.clientHeight;
+    ctx.setTransform(cvs.width/100, 0, 0, -cvs.height/100, 0, cvs.height);
+  };
+
+  /**
+   *
+   */
+  const redraw = () => {
+    ctx.clearRect(0, 0, cvs.clientWidth, cvs.clientHeight);
+    const numLines = 20;
+    const offset = 50/numLines;
+    const hueDelta = 360/numLines;
+    for (let i = 0; i <= numLines; ++i) {
+      const hue = i*hueDelta;
+      const dist = i*offset;
+      ctx.beginPath();
+      ctx.moveTo(50, 100 - dist);
+      ctx.lineTo(50 + dist, 50);
+      ctx.moveTo(50, 100 - dist);
+      ctx.lineTo(50 - dist, 50);
+      ctx.moveTo(50, dist);
+      ctx.lineTo(50 + dist, 50);
+      ctx.moveTo(50, dist);
+      ctx.lineTo(50 - dist, 50);
+      ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+      ctx.stroke();
+    }
+  };
+
+  window.addEventListener('resize', () => {
+    resizeCanvas();
+    redraw();
+  });
+  resizeCanvas();
+  redraw();
+});
