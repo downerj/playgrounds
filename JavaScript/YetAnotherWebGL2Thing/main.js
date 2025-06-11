54be4f6ee3e0d4fa6a3e28b34157ecd5f9de7d3e
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-06-10 23:08:47 -0600

    YetAnotherWebGL2Thing: Blue screen

--- JavaScript/YetAnotherWebGL2Thing/main.js
+++ JavaScript/YetAnotherWebGL2Thing/main.js
@@ -1,26 +1,43 @@
+import { setFramerateTimer } from "./timer.js";
+
 /**
- * 
+ * @param {HTMLCanvasElement} canvas
+ * @returns {WebGL2RenderingContext}
  */
-const resizeCanvas = () => {
-  /**
-   * @type {HTMLCanvasElement}
-   */
-  const canvas = document.getElementById('cvs');
+const init = (canvas) => {
+  const gl = canvas.getContext('webgl2');
+  if (!gl) {
+    throw 'Unable to get WebGL2 context';
+  }
+  return gl;
+};
+
+/**
+ * @param {WebGL2RenderingContext} gl
+ */
+const render = (gl) => {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0., .7, 1., 1.);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+};
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ */
+const resizeCanvas = (canvas) => {
   canvas.width = canvas.parentElement.clientWidth;
   canvas.height = canvas.parentElement.clientHeight;
 };
 
-/**
- * @param {Event} _event 
- */
-const window_resize = (_event) => resizeCanvas();
-
-/**
- * @param {Event} _event 
- */
-const document_DOMContentLoaded = (_event) => {
-  resizeCanvas();
-};
-
-document.addEventListener('DOMContentLoaded', document_DOMContentLoaded);
-window.addEventListener('resize', window_resize);
+document.addEventListener('DOMContentLoaded', (_event) => {
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  const canvas = document.getElementById('cvs');
+  window.addEventListener('resize', (_event) => resizeCanvas(canvas));
+  resizeCanvas(canvas);
+  const gl = init(canvas);
+  setFramerateTimer((_timestamp) => {
+    render(gl);
+  }, 1000/30);
+});
---
+++ JavaScript/YetAnotherWebGL2Thing/timer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {(timestamp: DOMHighResTimeStamp) => void} callback
+ * @param {number} interval
+ */
+export const setFramerateTimer = (callback, interval) => {
+  let previous = 0;
+  const onTick = (timestamp) => {
+    if (timestamp - previous >= interval) {
+      previous = timestamp;
+      callback(timestamp);
+    }
+    window.requestAnimationFrame(onTick);
+  };
+  window.requestAnimationFrame(onTick);
+};
