d2758e632a633048c85dd97cc67148c20e8cdebf
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-06-19 00:03:20 -0600

    YetAnotherWebGL2Thing: ProgramInfo class

--- JavaScript/YetAnotherWebGL2Thing/graphics.js
+++ JavaScript/YetAnotherWebGL2Thing/graphics.js
@@ -1,19 +1,76 @@
 import { Geometry } from "./geometry.js";
 
-class VertexArrayInfo {
+class ProgramInfo {
   /**
-   * @type {WebGLVertexArrayObject}
+   * @param {WebGL2RenderingContext} gl
+   * @param {string} vertexSource
+   * @param {string} fragmentSource
    */
-  #vertexArray;
-  /**
-   * @type {GLenum}
-   */
-  #drawMode;
-  /**
-   * @type {number}
-   */
-  #indexCount;
+  constructor(gl, vertexSource, fragmentSource) {
+    this.#gl = gl;
+    const vertexShader = this.#createShader(gl.VERTEX_SHADER, vertexSource);
+    const fragmentShader = this.#createShader(gl.FRAGMENT_SHADER, fragmentSource);
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const status = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!status) {
+      const programLog = gl.getProgramInfoLog(program);
+      console.error(`Program log: ${programLog}`);
+      const vertexLog = gl.getShaderInfoLog(vertexShader);
+      if (vertexLog != null) {
+        console.error(`Vertex log: ${vertexLog}`);
+      }
+      const fragmentLog = gl.getShaderInfoLog(fragmentShader);
+      if (fragmentLog != null) {
+        console.error(`Fragment log: ${fragmentLog}`);
+      }
+    }
+    gl.detachShader(program, vertexShader);
+    gl.deleteShader(vertexShader);
+    gl.detachShader(program, fragmentShader);
+    gl.deleteShader(fragmentShader);
+    if (!status) {
+      gl.deleteProgram(program);
+      throw 'Failed to create WebGL2 program';
+    }
+    this.#program = program;
+  };
 
+  get program() {
+    return this.#program;
+  }
+
+  useProgram() {
+    const gl = this.#gl;
+    gl.useProgram(this.#program);
+  }
+
+  /**
+   * @type {WebGL2RenderingContext}
+   */
+  #gl;
+  /**
+   * @type {WebGLProgram}
+   */
+  #program;
+
+  /**
+   * @param {GLenum} type
+   * @param {string} source
+   * @returns {WebGLShader}
+   */
+  #createShader(type, source) {
+    const gl = this.#gl;
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    return shader;
+  }
+}
+
+class VertexArrayInfo {
   /**
    *
    * @param {WebGLVertexArrayObject} vertexArray
@@ -37,6 +94,19 @@ class VertexArrayInfo {
   get indexCount() {
     return this.#indexCount;
   }
+
+  /**
+   * @type {WebGLVertexArrayObject}
+   */
+  #vertexArray;
+  /**
+   * @type {GLenum}
+   */
+  #drawMode;
+  /**
+   * @type {number}
+   */
+  #indexCount;
 }
 
 export class Graphics3D {
@@ -48,7 +118,8 @@ export class Graphics3D {
     if (!this.#gl) {
       throw 'Failed to get WebGL2 context';
     }
-    this.#program = this.#initProgram(
+    this.#programInfo = new ProgramInfo(
+      this.#gl,
       this.#mainVertexSource,
       this.#mainFragmentSource
     );
@@ -59,7 +130,7 @@ export class Graphics3D {
    */
   addObject(geometry) {
     const gl = this.#gl;
-    const program = this.#program;
+    const { program } = this.#programInfo;
     const { vertices, colors, indices } = geometry;
     const vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
@@ -97,7 +168,7 @@ export class Graphics3D {
     gl.enable(gl.DEPTH_TEST);
     gl.clearColor(0., .7, 1., 1.);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.useProgram(this.#program);
+    this.#programInfo.useProgram();
     gl.enable(gl.CULL_FACE);
     gl.cullFace(gl.BACK);
     gl.frontFace(gl.CCW);
@@ -112,56 +183,6 @@ export class Graphics3D {
     }
     gl.bindVertexArray(null);
   }
-
-  /**
-   * @param {GLenum} type
-   * @param {string} source
-   * @returns {WebGLShader}
-   */
-  #initShader(type, source) {
-    const gl = this.#gl;
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    return shader;
-  }
-
-  /**
-   * @param {string} vertexSource
-   * @param {string} fragmentSource
-   * @returns {WebGLProgram}
-  */
-  #initProgram(vertexSource, fragmentSource) {
-    const gl = this.#gl;
-    const vertexShader = this.#initShader(gl.VERTEX_SHADER, vertexSource);
-    const fragmentShader = this.#initShader(gl.FRAGMENT_SHADER, fragmentSource);
-    const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    const status = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (!status) {
-      const programLog = gl.getProgramInfoLog(program);
-      console.error(`Program log: ${programLog}`);
-      const vertexLog = gl.getShaderInfoLog(vertexShader);
-      if (vertexLog != null) {
-        console.error(`Vertex log: ${vertexLog}`);
-      }
-      const fragmentLog = gl.getShaderInfoLog(fragmentShader);
-      if (fragmentLog != null) {
-        console.error(`Fragment log: ${fragmentLog}`);
-      }
-    }
-    gl.detachShader(program, vertexShader);
-    gl.deleteShader(vertexShader);
-    gl.detachShader(program, fragmentShader);
-    gl.deleteShader(fragmentShader);
-    if (!status) {
-      gl.deleteProgram(program);
-      throw 'Failed to create WebGL2 program';
-    }
-    return program;
-  };
 
   /**
    * @param {GLenum} target
@@ -184,9 +205,9 @@ export class Graphics3D {
   #gl;
 
   /**
-   * @type {WebGLProgram}
+   * @type {ProgramInfo}
    */
-  #program;
+  #programInfo;
 
   /**
    * @type {WebGLBuffer[]}
