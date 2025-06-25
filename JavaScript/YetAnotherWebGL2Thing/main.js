1731066f37a555a91821d670df1d0740f153f18b
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-06-25 00:20:29 -0600

    YetAnotherWebGL2Thing: Camera rotate with mouse

--- JavaScript/YetAnotherWebGL2Thing/main.js
+++ JavaScript/YetAnotherWebGL2Thing/main.js
@@ -16,11 +16,24 @@ document.addEventListener('DOMContentLoaded', (_event) => {
    * @type {HTMLCanvasElement}
    */
   const canvas = document.getElementById('cvs');
+  canvas.addEventListener('click', (_event) => {
+    if (document.pointerLockElement != canvas) {
+      canvas.requestPointerLock();
+    }
+  });
   window.addEventListener('resize', (_event) => resizeCanvas(canvas));
   resizeCanvas(canvas);
   const gfx = new Graphics3D(canvas);
   const camera = new Camera();
   camera.moveZ(2.);
+  const rotateSpeed = 0.05;
+  window.addEventListener('mousemove', (event) => {
+    if (document.pointerLockElement != canvas) {
+      return;
+    }
+    camera.rotateX(event.movementY*rotateSpeed);
+    camera.rotateY(event.movementX*rotateSpeed);
+  });
   gfx.setCamera(camera);
   gfx.addObject(new SimpleTriangle());
   setFramerateTimer((_timestamp) => {
